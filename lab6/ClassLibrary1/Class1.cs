9b3eb32709da62b5eb62fdcde31fd10b84bc3cbb
XLJuice <35495987+XLJuice@users.noreply.github.com>
2018-11-22 22:08:29 +0200

    Add files via upload

---
+++ lab6/ClassLibrary1/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        private string surname;
+        public int age;
+        private string addres;
+        private int telephone;
+        private bool allow;
+        public string onhands;
+        public string needtoret;
+        public Person(int ID, string Name, string Surname, string Addres, int Telephone, bool Allow)
+        {
+            this.id = ID;
+            this.name = Name;
+            this.surname = Surname;
+            this.addres = Addres;
+            this.telephone = Telephone;
+            this.allow = Allow;
+
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 1)
+                    age = value;
+                else
+                    throw new InvalidAgeException();
+            }
+        }
+        public string Onhands
+        {
+            get { return onhands; }
+            set
+            {
+                onhands = value;
+            }
+        }
+        public string Needtoret
+        {
+            get { return needtoret; }
+            set
+            {
+                needtoret = value;
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Данi про користувача бiблiотеки: ");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Iм'я: " + name + ' ' + surname);
+            Console.WriteLine("Кiлькiсть рокiв: " + age);
+            Console.WriteLine("Адресса: " + addres);
+            Console.WriteLine("Телефон: +380" + telephone);
+            Console.WriteLine("Взятi книги: " + onhands);
+            Console.WriteLine("Треба повернути :" + needtoret);
+            Console.WriteLine(allow ? "Можна видавати книги" : "Не можна видавати книги");
+            Console.WriteLine();
+        }
+    }
+    //public class Old : Person
+    //{  
+    //    if (year < 60 ){throw new InvalidOldException
+    //}
+    //public Old(int ID, string Name, string Surname, int Year, string Addres, int Telephone, bool Allow) : base(ID, Name, Surname, Year, Addres, Telephone, Allow) { }
+
+}
+public class PersonException : Exception
+{
+    public PersonException(string messange) : base(messange) { }
+}
+
+public class InvalidAgeException : PersonException
+{
+    public InvalidAgeException() : base("Ви вказали не коректний вік") { }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace Lab5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Andrey = new Person(156948, "Andrey", "Cherbikov", "Pr.Miru 43B", 0935948952, false)
+            {
+                age= 30,
+                onhands = "Шерлок Холмс,Буратiно",
+                needtoret = "Буратiно"
+            };
+            Andrey.PrintInfo();
+            Person Yuri = new Person(759615, "Yuri", "Merdov", "st.Nikolskaya", 0937628439, true)
+            {
+                age = 52,
+                onhands = "Мобi дiк, Буквар, Сяйво",
+                needtoret = ""
+            };
+            Yuri.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
